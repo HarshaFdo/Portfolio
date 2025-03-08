a9a24200dd1d91954212c74d1d8f05e29c7101fd
HarshaFdo <fernando.harsha2016@gmail.com>
2025-03-08 22:39:04 +0530

    edit tailwind css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Variable } from "lucide-react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const JetBrains_Mono = JetBrains_Mono({ 
+  subsets: ["latin"], 
+  weight: ["100", "200", "300","400","500","600","700","800"],
+  Variable: '--font-jetbrainsMono'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={JetBrains_Mono.variable}>
         {children}
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,53 @@
 const { Container } = require('lucide-react');
 
 /** @type {import('tailwindcss').Config} */
-module.export = {
+module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*{js,jsx}',
-    '/components/**/*.{js,jsx}',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
     './app/**/*.{js,jsx}',
     './src/**/*.{js,ts,jsx}'
     ],
   prefix: "",
   theme: {
-    Container: {
+    container: {
       center: true,
       padding: "15px",
-    screens: {
-       
-      },
     },
+    screens: {
+       sm: "640px",
+       md: "768px",
+       lg: "960px",
+       xl: "1200px",
+      },
     extend: {
+      fontFamily: {
+        primary: "var(--font-jetbrainsMono)",
+      },
+      colors: {
+        primary: '#1c1c22',
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+      },
+
       keyframes: {
-        "accordian-down": {
+        "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height" },
-        }
-      }
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" }, 
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     }
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
